a28134f6801d10f9759c098d34d2f843e1ab8cdf
Rodolfo135 <galvanrodolfo17@gmail.com>
2024-04-04 22:39:09 -0700

    Update formatter.py

--- data_formatting/formatter.py
+++ data_formatting/formatter.py
@@ -37,4 +37,19 @@ def get_files(db_name, usr, psswd, host, port):
   # Return list
   return json_files
 
+def join_files(json_list):
+  """
+  This function takes in a list of previously joined json objects and combines into a single .json file
+  """
 
+  # Empty list to keep python objects
+  python_objs = []
+
+  # Load each .json file into pyton obj
+  for jsonfile in json_list:
+    with open(jsonfile, 'r') as f:
+      python_objs.append(json.load(f))
+
+  # Dump objects into a json file
+  with open("combined_data.json", 'w') as f:
+    json.dump(python_objs, f, indent=4)
