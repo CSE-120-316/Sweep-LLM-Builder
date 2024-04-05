bd248b836534b32140511a053844bd71030aaf47
Rohit-R-Rao <104187252+Rohit-R-Rao@users.noreply.github.com>
2024-04-05 12:31:27 -0700

    Update hf_finetune.py

--- LLM_Code/hf_finetune.py
+++ LLM_Code/hf_finetune.py
@@ -1,5 +1,3 @@
-# Rohit Rao April 4th, 2024
-# Description: Basic inference code for interacting with finetuned model.
 import os
 import torch
 from datasets import load_dataset
@@ -11,35 +9,36 @@ from huggingface_hub import login
 
 # torch check
 print("Torch Availability: ",torch.cuda.is_available()) # ignore
-
 login(token='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') # My key to access official llama2.
-
-# Force garbage collection
 gc.collect()
  
 
 # Define model, dataset, and new model name
-base_model = "meta-llama/Llama-2-7b-chat-hf" # Official Meta Llama2
-datasetName = "AdiOO7/llama-2-finance"
+# Huggingface Dataset Setup:
+
+#hf_dataSetName = "AdiOO7/llama-2-finance" # THis is for HF Dataset
+#dataset = load_dataset(hf_dataSetName, split="train")
 # Working Datasets:
 # "AdiOO7/llama-2-finance"
 # "mlabonne/guanaco-llama2-1k"
 
+#
+base_model = "meta-llama/Llama-2-7b-chat-hf" # Official Meta Llama2
+datasetName = "llama-2-finance-local" # This is for Local Dataset
 new_model = "llama-2-7b-chat-hf" + datasetName
 
-
-dataset = load_dataset(datasetName, split="train")
-
+# Need to load local dataset.
+local_dataset = load_dataset("json", data_files="datasets/finance_data.json", split="train")
 
 compute_dtype = getattr(torch, "float16")
 quant_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=compute_dtype, bnb_4bit_use_double_quant=False)
 
-
+# Loading the pretrained model.
 model = AutoModelForCausalLM.from_pretrained(base_model, quantization_config=quant_config, device_map={"": 0})
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 
-
+# Using autotokenizer from hf for simplicity
 tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
@@ -51,7 +50,7 @@ outputdir = "./results/" + datasetName
 
 training_params = TrainingArguments(output_dir=outputdir, num_train_epochs=1, per_device_train_batch_size=4, gradient_accumulation_steps=1, optim="paged_adamw_32bit", save_steps=25, logging_steps=25, learning_rate=2e-4, weight_decay=0.001, fp16=False, bf16=False, max_grad_norm=0.3, max_steps=-1, warmup_ratio=0.03, group_by_length=True, lr_scheduler_type="constant", report_to="tensorboard")
 
-trainer = SFTTrainer(model=model, train_dataset=dataset, peft_config=peft_params, dataset_text_field="text", max_seq_length=None, tokenizer=tokenizer, args=training_params, packing=False)
+trainer = SFTTrainer(model=model, train_dataset=local_dataset, peft_config=peft_params, dataset_text_field="text", max_seq_length=None, tokenizer=tokenizer, args=training_params, packing=False)
 
 
 gc.collect()
