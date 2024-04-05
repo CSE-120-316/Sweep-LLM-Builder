1dba71d7334048178c72cebb23cb56b65938acf4
Rodolfo135 <galvanrodolfo17@gmail.com>
2024-04-04 19:17:09 -0700

    Update formatter.py

--- data_formatting/formatter.py
+++ data_formatting/formatter.py
@@ -4,3 +4,22 @@ import json
 import pyarrow as pa
 import pyarrow.parquet as pq
 from pyarrow import json
+
+def formatter(db_name, usr, psswd, host, port):
+  """
+  This function interacts with the provided postgres database in order to retrieve the .json files
+  that are being stored in its table. These .json files are then loaded into a list.
+  """
+
+  # Connect to DB
+  conn = psycopg2.connect(db_name=db_name, user=usr, password=psswd, host=host, port=port)
+  
+  cursor = conn.cursor()
+
+  # SQL query to get the .json files
+  query = "Retrieve json_files"
+
+  cursor.execute(query)
+
+  # Retrieve info from query
+  rows = cursor.fetchall()
