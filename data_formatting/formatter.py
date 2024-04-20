fadbea478dece66b3987eb79e47580f7c12c9b0e
Rodolfo135 <galvanrodolfo17@gmail.com>
2024-04-19 22:13:59 -0700

    Update formatter.py
    Deleted join_files and added the functionality for saving the dataset

--- data_formatting/formatter.py
+++ data_formatting/formatter.py
@@ -30,24 +30,5 @@ def save_combined_dataset(output_file, dataset):
   """
   Save the combined dataset as a JSON file.
   """ 
-
-def join_files(json_list):
-  """
-  This function takes in a list of previously joined json objects and combines into a single .json file
-  """
-
-  # Empty list to keep python objects
-  python_objs = []
-
-  # Load each .json file into pyton obj
-  for jsonfile in json_list:
-    with open(jsonfile, 'r') as f:
-      python_objs.append(json.load(f))
-
-  # Dump objects into a json file
-  with open("combined_data.json", 'w') as f:
-    json.dump(python_objs, f, indent=4)
-
-
-# No longer need function to streamline process for converting .json files to apache parquet. This is because
-# in the finetuning code when we read from .json we will already convert to parquet automatically.
+  with open(output_file, 'w', encoding='utf-8') as file:
+    json.dump(dataset, file, ensure_ascii=False, indent=2)
